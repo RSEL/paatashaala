991641ca53a43fb7b7c0c4b6f3d650de8630c8d5
Ravikant Shukla <ravikantshukla5@gmail.com>
2013-07-26 18:03:51 +0530

    rename the variable

--- paataprot/src/main/java/com/ramselabs/education/security/Manager.java
+++ paataprot/src/main/java/com/ramselabs/education/security/Manager.java
@@ -2,8 +2,8 @@ package com.ramselabs.education.security;
 
 public class Manager {
 	public static void main(String[] args) {
-		String  s1=EncryptDecryptUtil.encrypt("root");
-		System.out.println(s1);
+		String str = EncryptDecryptUtil.encrypt("root");
+		System.out.println(str);
 	}
 
 }
