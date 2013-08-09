b1792b85b502a40b142fd9173d3d8675d8a7be87
arun.krish@ramselabs.com <arun.krish@ramselabs.com>
2013-08-09 14:21:18 +0530

    Updated pom.xml and added dbscript for insert

---
+++ paataprot/dbscripts/insertRecords.sql
@@ -0,0 +1,5 @@
+insert into user(userId,password,username) values(2,'1234','ravikant');
+insert into user(userId,password,username) values(3,'12345','shashi');
+insert into share(shareId, name, userId) values(1,'Ravikant',2);
+insert into share(shareId, name, userId) values(2,'ArunKrish',1);
+insert into share(shareId, name, userId) values(3,'Shashikant',3);
